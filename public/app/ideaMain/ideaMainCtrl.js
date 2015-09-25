9cb8d6b7f1bb7a834c24fae93780fdefa23f6807
Ting Deng <maidenbelize@gmail.com>
2015-09-25 15:26:25 -0700

    [feat] add modal to mainIdeas view

--- public/app/ideaMain/ideaMainCtrl.js
+++ public/app/ideaMain/ideaMainCtrl.js
@@ -3,9 +3,10 @@ angular.module('myApp.ideaMain', [])
 .controller('IdeaMainCtrl', ['$scope', '$http', 'data', function($scope, $http, data){
   var vm = this;
 
-  vm.$on('gotIdeas', function (event, ideas){
-    console.log("ideas retrieved!", ideas);
+  data.getIdeasData()
+
+  $scope.$on('gotIdeas', function (event, ideas){
+   console.log("ideas retrieved!", ideas);
   })
   
-
 }])
