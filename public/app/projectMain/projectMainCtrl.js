e79d1c529363b6dfbc98834a6f21f8f6505dac23
codeNameAtlas <anavarrete255@gmail.com>
2015-09-25 16:04:18 -0700

    [feat] - add modal button to projectMain page

--- public/app/projectMain/projectMainCtrl.js
+++ public/app/projectMain/projectMainCtrl.js
@@ -2,10 +2,13 @@ angular.module('myApp.projectMain', [])
 
 .controller('ProjectMainCtrl', ['$scope', '$http', function($scope, $http) {
   var vm = this;
+  console.log("this is data", data);
 
-  // vm.$on('gotProjects')
-  //   .then(function(projects) {
-  //     console.log('projects retrieved', projects);
-  //   })
+  data.getProjectsData();
+
+  $scope.$on('gotProjects')
+    .then(function(projects) {
+      console.log('projects retrieved', projects);
+    })
 }])
 
