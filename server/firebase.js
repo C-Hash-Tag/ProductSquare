c3d8d3357ee6af6fb429a220185a489f0a6e3ba7
Sin Wai Lam <sinwailam193@hotmail.com>
2015-09-24 19:19:11 -0700

    [feat] added firebase schema into database, going to mover everything from back end to front end

--- server/firebase.js
+++ server/firebase.js
@@ -1,1 +1,40 @@
 var Firebase = require('firebase');
+
+var myDataRef = new Firebase('https://productsquare.firebaseio.com/');
+
+var submitProject = exports.submitProject = function(data){
+  myDataRef.child('projects/').push({
+    name: data.name,
+    video: data.video,
+    images: data.images,
+    description: data.description,
+    githubRepo: data.githubRepo,
+    users: data.users,
+    nonusers: data.nonusers,
+    likes: data.likes,
+    dateCreated: data.dateCreated
+  });
+};
+
+var submitIdeas = 
+
+// myDataRef.child('users/').push({
+//   email: "ceo@chashtag.com",
+//   name: "Aaron",
+//   password: "123",
+//   github: "something@github.com",
+//   projects: {"0": "xyz"},
+//   ideas: {"0": "idea1"},
+//   profilePicture: "picturelink",
+//   "liked-projects": {"0": "project1id"},
+//   "liked-ideas": {"0": "idea1id"}
+// })
+
+// myDataRef.child('ideas/').push({
+//   name: "Idea to make coffee app",
+//   description: "making an app about coffee",
+//   likes: 4,
+//   name: "Aaron",
+//   dateCreated: "10/25/2015"
+// })
+
--- server/server.js
+++ server/server.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var app = express();
 var port = process.env.Port || 3000;
+var firebase = require('./firebase.js');
+
+var dummyData = {
+  name: "Building a coffee app",
+  video: {"0":"youtubeURL", "1":"youtubeURL"},
+  images: {'0':"imgeURL"},
+  description: "this app help makes coffee",
+  githubRepo: "whatever@url.com",
+  users: {'0':"userId", '1':"userId"},
+  nonusers: {'0':"Dylan", '1':"Aaron", '2':"Ting"},
+  likes: 3,
+  dateCreated: "10/25/2015"
+}
+
+var dummyData1 = {
+  name: "Idea to make coffee app",
+  description: "making an app about coffee",
+  likes: 4,
+  name: "Aaron",
+  dateCreated: "10/25/2015"
+}
 
 app.use(express.static("../public"));
 
+app.post('/projects', function(req, res){
+  firebase.submitProject(dummyData);
+});
+
+app.post('/ideas', function(req, res){
+  firebase.submitIdeas(dummyData1);
+})
+
 app.listen(port);
 console.log('Server listening on ' + port);
+
