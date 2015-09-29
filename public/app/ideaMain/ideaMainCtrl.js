a41444ee5d5563ef3f36c2de407130c0c91d124c
Ting Deng <maidenbelize@gmail.com>
2015-09-29 09:51:24 -0700

    [feat] - each idea card flips individually when clicked

--- public/app/ideaMain/ideaMainCtrl.js
+++ public/app/ideaMain/ideaMainCtrl.js
@@ -1,9 +1,11 @@
 angular.module('myApp.ideaMain', [])
 
-.controller('IdeaMainCtrl', ['$scope', '$http', 'data', function($scope, $http, data){
+.controller('IdeaMainCtrl', ['$scope', '$http', 'data', '$log', function($scope, $http, data, $log){
   var vm = this;
 
-  data.getIdeasData()
+  data.getIdeasData();
+
+  vm.newIdeas = [{'text':'a'}, {'text': 'b'}];
 
   $scope.$on('gotIdeas', function (event, ideas){
    console.log("ideas retrieved!", ideas);
@@ -16,15 +18,20 @@ angular.module('myApp.ideaMain', [])
     //TODO: submit to firebase
   }
 
-  $scope.flippedClass = "card";
 
-  $scope.toggleClass = function(){
-    if ($scope.flippedClass == "card flipped") {
-      $scope.flippedClass = "card";
-    }
-    else {
-      $scope.flippedClass = "card flipped";
-    } 
+  //vm.options = {flippedClass : 'card', pageSize: 12};
+  //vm.flippedClass = "card";
+
+  // ng-click="toggleClass($index)"
+  vm.toggleClass = function($index){
+    vm.ideas[$index].flipped = !vm[$index].flipped;
+    //if ($scope.idea[$index].flipped === "card flipped") {
+    //  $scope.flippedClass = "card";
+    //}
+    //else {
+    //  $scope.flippedClass = "card flvped";
+    //} 
   }
-
+  
+  return vm;
 }])
