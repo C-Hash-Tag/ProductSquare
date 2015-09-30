d612df5c10800e82f28baf4a1efc6cc09274023e
Dylan Wright <dylansamuelwright@gmail.com>
2015-09-29 19:36:42 -0700

    [bug] made the nav bar reappear

--- public/app/navbar/navbar.js
+++ public/app/navbar/navbar.js
@@ -1,12 +1,15 @@
-angular.module("myApp").directive("navBar", ['$window', function($window) {
+angular.module("myApp").directive("navBar", ['$window', "data", function($window, data) {
+
   return {
     restrict: 'E',
     templateUrl: 'app/navbar/navbar.html',
     link: function(scope, elem, attrs) {
-      /*
-      The below methods are used to modify the navbar routes available to a
-      specific user based on their privilege and current url path
-      */
+      
+      //put the newuser function here.  
+      scope.newUser = function() {
+        console.log("blah blah");
+      };
+    
     }
   };
 }]);
