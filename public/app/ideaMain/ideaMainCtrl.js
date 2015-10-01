e6d36fe02a09c1738b2fa596fa62c933ca83cec3
Ting Deng <maidenbelize@gmail.com>
2015-09-30 17:04:44 -0700

    rebased files

--- public/app/ideaMain/ideaMainCtrl.js
+++ public/app/ideaMain/ideaMainCtrl.js
@@ -1,44 +1,4 @@
-// angular.module('myApp.ideaMain', [])
-
-// .controller('IdeaMainCtrl', ['$scope', '$http', 'data', '$log', function($scope, $http, data, $log){
-//   var vm = this;
-  
-//   //NOTE: set the listener before you get the dat
-//   $scope.$on('gotIdeas', function (event, ideas){
-//    console.log("ideas retrieved!", ideas);
-//    vm.newIdeas = ideas;
-//    $scope.$apply();
-//   })
-
-//   vm.getIdeasData = function(){
-//     console.log("inside get Ideas Data")
-//     data.getIdeasData();
-//   }
-
-//   vm.getIdeasData()
-
-//   vm.newIdeas; 
-
-//   console.log("inside initScope");
-
-
-//   //TODO: add the username too
-//   vm.postIdea = function(name, description, username){
-//     data.createIdea(name, description, 'username');
-//   }
-
-//   //FOR TESTING PURPOSES  
-//   vm.postIdea("trello222", "trello for wizards", "ting");
-//   console.log("in ideas")
-
-//   return vm;
-
-// }]);
-
-
-// OPTION 2: 
-
-angular.module('myApp.ideaMain', ['ui.bootstrap'])
+angular.module('myApp.ideaMain', [])
 
 .controller('IdeaMainCtrl', ['$scope', '$http', 'data', '$log', function($scope, $http, data, $log){
   var vm = this;
