0f01277e3e44a832c39bf2472d9361b4475d6f74
Christopher Kim <christopherjhkim@gmail.com>
2015-10-01 14:50:29 -0700

    [feature] Allow to change the navbar view based on log in status

--- public/app/navbar/navbar.js
+++ public/app/navbar/navbar.js
@@ -5,13 +5,23 @@ angular.module("myApp").directive("navBar", ['$window', "data", function($window
     templateUrl: 'app/navbar/navbar.html',
     link: function(scope, elem, attrs) {
       scope.submission = false;
+      scope.loggedIn = false;
       //put the newuserSubmit function here.
       scope.newUserSubmit = function(username, realName, email, password) {
         data.createUser(email, password, username, realName);
         scope.submission = true;
+        // scope.realName = '';
       };
       scope.close = function() {
         scope.submission = false;
+      };
+      scope.login = function(email, password) {
+        data.login(email, password);
+        scope.loggedIn = true;
+      };
+      scope.logout = function() {
+        console.log("LOGOUT");
+        scope.loggedIn = false;
       };
     }
   };
