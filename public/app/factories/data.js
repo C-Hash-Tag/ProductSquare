4f6e19b3e0959fb0be46e8cb176945e71e308331
codeNameAtlas <anavarrete255@gmail.com>
2015-10-03 14:15:23 -0700

    [feat] - bug fix on projName form submit

--- public/app/factories/data.js
+++ public/app/factories/data.js
@@ -60,13 +60,14 @@ angular.module('myApp.data', [])
     };
 
     // Collect project data from createProject and store it in Firebase
-    factory.createProject = function(projDesc, githubUrl, projName, projUrl, projectImage){
+    factory.createProject = function(projDesc, githubUrl, projName, projUrl, projID, projectImage){
       // Store the project data in Firebase
       Ref.child('projects').child(projName).set({
         description: projDesc,
         githubRepo: githubUrl,
         projName: projName,
         projUrl: projUrl,
+        projID: projID,
         date: currentDate(),
         userID: localStorage.userID,
         projectImage: projectImage
