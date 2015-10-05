7802eb0e922103b18482067b6f514b937ae33da9
Sin Wai Lam <sinwailam193@hotmail.com>
2015-10-05 10:22:21 -0700

    refactor vm to  in idea page

--- public/app/ideaMain/ideaMainCtrl.js
+++ public/app/ideaMain/ideaMainCtrl.js
@@ -1,46 +1,32 @@
 angular.module('myApp.ideaMain', [])
 
 .controller('IdeaMainCtrl', ['$scope', '$http', 'data', '$log', 'auth', function($scope, $http, data, $log, auth){
-  var vm = this;
-  
   //NOTE: set the listener before you get the dat
   $scope.$on('gotIdeas', function (event, ideas){
    console.log("ideas retrieved!", ideas);
-   vm.newIdeas = ideas;
+   $scope.newIdeas = ideas;
    $scope.$apply();
   })
 
-  vm.getIdeasData = function(){
+  $scope.getIdeasData = function(){
     console.log("inside get Ideas Data")
     data.getIdeasData();
   }
 
-  vm.getIdeasData()
-
-  vm.newIdeas; 
-
-  console.log("inside initScope");
-
+  $scope.getIdeasData()
 
   //TODO: add the username too
-  vm.postIdea = function(name, description){
-    data.createIdea(name, description, localStorage.userID);
+  $scope.postIdea = function(ideaName, description){
+    data.createIdea(ideaName, description, localStorage.userID);
   }
 
-  vm.like = function(userID, ideaName){
+  $scope.like = function(ideaName){
     //update css of the like button
     
     //update the database 
-    data.updateLike(userID, ideaName);
+    data.updateLike(localStorage.userID, ideaName);
       //TODO: if username liked it before, remove her; if username hasn't, add her
       //add/ remove idea in user's liked ideas 
   }
-
-  //FOR TESTING PURPOSES  
-  // vm.postIdea("trello222", "trello for wizards", "ting");
-  console.log("in ideas")
-
-
-  return vm;
 
 }])
