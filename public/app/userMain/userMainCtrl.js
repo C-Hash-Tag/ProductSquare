6988919f0c13e49c79d8ef29c4b4dabfef85e053
Ting Deng <maidenbelize@gmail.com>
2015-10-06 15:04:29 -0700

    rebase files

--- public/app/userMain/userMainCtrl.js
+++ public/app/userMain/userMainCtrl.js
@@ -1,7 +1,5 @@
 angular.module('myApp.UserMain', [])
 
-<<<<<<< HEAD
-
 .controller('UserMainCtrl', ['$scope', '$http', 'data', '$routeParams', 'imageUpload', function($scope, $http, data, $routeParams, imageUpload){
 
   var truncateText = function(text) {
