cef6cb4a1621c1913f91d0018f429e183b7c1efa
Sin Wai Lam <sinwailam193@hotmail.com>
2015-10-13 11:00:36 -0700

    [refactor] made changes to the tests and specs

--- public/tests/spec.js
+++ public/tests/spec.js
@@ -2,20 +2,14 @@ describe('Data Factory', function(){ //describe your object type
     
     var data;
 
-    beforeEach(module('myApp.data'));
-    beforeEach(inject(function (_data_) {
-        data = _data_;
-        console.log(data);
-    }));
-
-    describe('Constructor', function () {
-
-        it('testing testing', function () {
-            var method = data.createUser
-            expect(method).to.exist;
+    beforeEach(function (){
+        module('myApp.data');
+        inject(function(_data_){
+            data = _data_;
         });
-
     });
     
-
+    it('it should have a createUser function', function(){
+        expect(angular.isFunction(data.createUser)).toBe(true);
+    });
 });
